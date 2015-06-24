6ba31480a7eb5eee684d0b9d428c334c9fcb217c
Sambhav <sambhavk11@gmail.com>
2015-06-24 17:19:13 +0800

    register screen in progress

--- app/src/main/java/com/heard_knowe/overnight/Register.java
+++ app/src/main/java/com/heard_knowe/overnight/Register.java
@@ -1,12 +1,13 @@
 package com.heard_knowe.overnight;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Register extends ActionBarActivity {
+public class Register extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
